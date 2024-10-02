880fb4aad2a8775dbaaf69139621ff54b9bfc141
FathanRaedaya <fy21fnr@leeds.ac.uk>
2024-10-02 22:14:36 +0100

    HTML, TailwindCSS, Javascript setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    },
+    fontFamily: {
+      'it': ['it', 'sans-serif'],
+      'mont': ['mont', 'sans-serif'],
+      'montM': ['montM', 'sans-serif'],
+      'montL': ['montL', 'sans-serif'],
+      'lmr': ['lmr', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
